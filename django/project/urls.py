a7179ec8d0b96ac22e97b0a29317b31684b6352b
DmitryDubovikov <110840154+DmitryDubovikov@users.noreply.github.com>
2023-04-03 18:26:30 +0300

    swagger docs added, readme updated

--- django/project/urls.py
+++ django/project/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 from students.views import StudentViewSet
 from django.conf import settings
@@ -12,4 +13,9 @@ router.register(r"students", StudentViewSet, basename="students")
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
+    path("api/swagger/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/swagger/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
